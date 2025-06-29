0d98a3eb6b2c53b6c8702d743981aa663dc969dd
SS634-rs <satoru634ca@gmail.com>
2025-06-29 23:31:56 +0900

    コピーコンストラクタ＆コメント追加

--- MCToolsCommonLib/BaseData/PointRange.cs
+++ MCToolsCommonLib/BaseData/PointRange.cs
@@ -7,24 +7,59 @@ using System.Threading.Tasks;
 
 namespace MCToolsCommonLib.BaseData
 {
+    /// <summary>
+    /// 2D座標と範囲を表すクラス
+    /// </summary>
     public class PointRange
     {
+        /// <summary>
+        /// 2D座標を表すPointXZオブジェクト
+        /// </summary>
         public PointXZ Point { get; set; }
+
+        /// <summary>
+        /// 範囲を表す整数値
+        /// </summary>
         public int Range { get; set; }
 
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="x">X座標</param>
+        /// <param name="z">Z座標</param>
+        /// <param name="range">範囲</param>
         public PointRange(int x = 0, int z = 0, int range = int.MaxValue)
         {
             Point = new PointXZ(x, z);
             Range = range;
         }
 
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="point">2D座標</param>
+        /// <param name="range">範囲</param>
         public PointRange(PointXZ point, int range = int.MaxValue)
         {
             Point = point;
             Range = range;
         }
 
-        // 当たり判定(矩形)
+        /// <summary>
+        /// コピーコンストラクタ
+        /// </summary>
+        /// <param name="pointRange">2D座標と範囲</param>
+        public PointRange(PointRange pointRange)
+        {
+            Point = new PointXZ(pointRange.Point);
+            Range = pointRange.Range;
+        }
+
+        /// <summary>
+        /// 当たり判定(矩形)
+        /// </summary>
+        /// <param name="target">対象の2D座標と範囲</param>
+        /// <returns>当たり判定の結果</returns>
         public bool IsCollisionWithRect(PointRange target)
         {
             PointXZ baseLT = new PointXZ(Point);
@@ -39,7 +74,12 @@ namespace MCToolsCommonLib.BaseData
             return baseLT.X <= targetRB.X && targetLT.X <= baseRB.X && baseLT.Z <= targetRB.Z && targetLT.Z <= baseRB.Z;
         }
 
-        // 当たり判定(矩形)
+        /// <summary>
+        /// 当たり判定(矩形)
+        /// </summary>
+        /// <param name="targetLT">左上の3D座標</param>
+        /// <param name="targetRB">右下の3D座標</param>
+        /// <returns>当たり判定の結果</returns>
         public bool IsCollisionWithRect(Point3D targetLT, Point3D targetRB)
         {
             PointXZ baseLT = new PointXZ(Point);
@@ -49,14 +89,23 @@ namespace MCToolsCommonLib.BaseData
             return baseLT.X <= targetRB.X && targetLT.X <= baseRB.X && baseLT.Z <= targetRB.Z && targetLT.Z <= baseRB.Z;
         }
 
-        // 当たり判定(円形)
+        /// <summary>
+        /// 当たり判定(円形)
+        /// </summary>
+        /// <param name="target">対象の2D座標と範囲</param>
+        /// <returns>当たり判定の結果</returns>
         public bool IsCollisionWithCircle(PointRange target)
         {
             double dist = CommonLib.CalcDistance2D(Point, target.Point);
             return dist <= Range;
         }
 
-        // 当たり判定(円形)
+        /// <summary>
+        /// 当たり判定(円形)
+        /// </summary>
+        /// <param name="targetLT">左上の3D座標</param>
+        /// <param name="targetRB">右下の3D座標</param>
+        /// <returns>当たり判定の結果</returns>
         public bool IsCollisionWithCircle(Point3D targetLT, Point3D targetRB)
         {
             double distLT = CommonLib.CalcDistance2D(Point, targetLT);
@@ -64,6 +113,10 @@ namespace MCToolsCommonLib.BaseData
             return distLT <= Range || distRB <= Range;
         }
 
+        /// <summary>
+        /// 座標を文字列として表現する
+        /// </summary>
+        /// <returns>文字列</returns>
         public override string ToString()
         {
             return $"X: {Point.X}, Z:{Point.Z}, Range:{Range}";
