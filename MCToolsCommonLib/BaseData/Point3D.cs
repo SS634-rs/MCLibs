8127d017d3a22c58a73ddf142d0d400dbceb4b9c
SS634-rs <ryo-sato634@star2.gmobb.jp>
2025-06-29 23:31:56 +0900

    コピーコンストラクタ＆コメント追加

--- MCToolsCommonLib/BaseData/Point3D.cs
+++ MCToolsCommonLib/BaseData/Point3D.cs
@@ -6,12 +6,29 @@ using System.Threading.Tasks;
 
 namespace MCToolsCommonLib.BaseData
 {
+    /// <summary>
+    /// 3D座標を表すクラス
+    /// </summary>
     public class Point3D
     {
+        /// <summary>
+        /// X座標
+        /// </summary>
         public int X { get; set; }
+
+        /// <summary>
+        /// Y座標
+        /// </summary>
         public int Y { get; set; }
+
+        /// <summary>
+        /// Z座標
+        /// </summary>
         public int Z { get; set; }
 
+        /// <summary>
+        /// デフォルトコンストラクタ
+        /// </summary>
         public Point3D()
         {
             X = 0;
@@ -19,6 +36,12 @@ namespace MCToolsCommonLib.BaseData
             Z = 0;
         }
 
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="x">X座標</param>
+        /// <param name="y">Y座標</param>
+        /// <param name="z">Z座標</param>
         public Point3D(int x = 0, int y = 0, int z = 0)
         {
             X = x;
@@ -26,6 +49,23 @@ namespace MCToolsCommonLib.BaseData
             Z = z;
         }
 
+        /// <summary>
+        /// コピーコンストラクタ
+        /// </summary>
+        /// <param name="point">3D座標</param>
+        public Point3D(Point3D point)
+        {
+            X = point.X;
+            Y = point.Y;
+            Z = point.Z;
+        }
+
+        /// <summary>
+        /// 座標の最大値を更新
+        /// </summary>
+        /// <param name="x">X座標</param>
+        /// <param name="y">Y座標</param>
+        /// <param name="z">Z座標</param>
         public void Max(int x, int y, int z)
         {
             X = Math.Max(x, X);
@@ -33,6 +73,12 @@ namespace MCToolsCommonLib.BaseData
             Z = Math.Max(z, Z);
         }
 
+        /// <summary>
+        /// 座標の最小値を更新
+        /// </summary>
+        /// <param name="x">X座標</param>
+        /// <param name="y">Y座標</param>
+        /// <param name="z">Z座標</param>
         public void Min(int x, int y, int z)
         {
             X = Math.Min(x, X);
@@ -40,6 +86,12 @@ namespace MCToolsCommonLib.BaseData
             Z = Math.Min(z, Z);
         }
 
+        /// <summary>
+        /// 座標を指定された値だけシフトする
+        /// </summary>
+        /// <param name="x">X座標の移動量</param>
+        /// <param name="y">Y座標の移動量</param>
+        /// <param name="z">Z座標の移動量</param>
         public void Shift(int x, int y, int z)
         {
             X += x;
@@ -47,12 +99,20 @@ namespace MCToolsCommonLib.BaseData
             Z += z;
         }
 
+        /// <summary>
+        /// すべての座標を指定された値だけシフトする
+        /// </summary>
+        /// <param name="shift">移動量</param>
         public void ShiftAll(int shift)
         {
             Shift(shift, shift, shift);
             return;
         }
 
+        /// <summary>
+        /// 座標を文字列として表現する
+        /// </summary>
+        /// <returns>文字列</returns>
         public override string ToString()
         {
             return $"X: {X}, Y: {Y}, Z:{Z}";
--- MCToolsCommonLib/BaseData/PointXZ.cs
+++ MCToolsCommonLib/BaseData/PointXZ.cs
@@ -6,53 +6,98 @@ using System.Threading.Tasks;
 
 namespace MCToolsCommonLib.BaseData
 {
+    /// <summary>
+    /// 2D座標を表すクラス
+    /// </summary>
     public class PointXZ
     {
+        /// <summary>
+        /// X座標
+        /// </summary>
         public int X { get; set; }
+
+        /// <summary>
+        /// Z座標
+        /// </summary>
         public int Z { get; set; }
 
+        /// <summary>
+        /// デフォルトコンストラクタ
+        /// </summary>
         public PointXZ()
         {
             X = 0;
             Z = 0;
         }
 
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="x">X座標</param>
+        /// <param name="z">Z座標</param>
         public PointXZ(int x = 0, int z = 0)
         {
             X = x;
             Z = z;
         }
 
+        /// <summary>
+        /// コピーコンストラクタ
+        /// </summary>
+        /// <param name="point">2D座標</param>
         public PointXZ(PointXZ point)
         {
             X = point.X;
             Z = point.Z;
         }
 
+        /// <summary>
+        /// 2D座標の最大値を更新
+        /// </summary>
+        /// <param name="x">X座標</param>
+        /// <param name="z">Z座標</param>
         public void Max(int x, int z)
         {
             X = Math.Max(x, X);
             Z = Math.Max(z, Z);
         }
 
+        /// <summary>
+        /// 2D座標の最小値を更新
+        /// </summary>
+        /// <param name="x">X座標</param>
+        /// <param name="z">Z座標</param>
         public void Min(int x, int z)
         {
             X = Math.Min(x, X);
             Z = Math.Min(z, Z);
         }
 
+        /// <summary>
+        /// 2D座標を指定された値だけシフトする
+        /// </summary>
+        /// <param name="x">X座標の移動量</param>
+        /// <param name="z">Z座標の移動量</param>
         public void Shift(int x, int z)
         {
             X += x;
             Z += z;
         }
 
+        /// <summary>
+        /// すべての座標を指定された値だけシフトする
+        /// </summary>
+        /// <param name="shift">移動量</param>
         public void ShiftAll(int shift)
         {
             Shift(shift, shift);
             return;
         }
 
+        /// <summary>
+        /// 座標を文字列として表現する
+        /// </summary>
+        /// <returns>文字列</returns>
         public override string ToString()
         {
             return $"X: {X}, Z:{Z}";
